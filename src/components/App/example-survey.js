21d6336e738e210562ae75e56a64ee92590b86eb
Greyson Elkins <greysonelkins@gmail.com>
2021-02-17 14:08:54 -0700

    Create beginning of question component

--- src/components/App/example-survey.js
+++ src/components/App/example-survey.js
@@ -1,10 +1,27 @@
 const useCase = {
   questions: [
-    {question: "What role best describes your clients?", answerType: "option", options: ["Paralegals", "Lawyers"]},
-    {question: "Where do your clients typically work?", answerType: "option", options: ["law firms", "inhouse legal departments"]},
-    {question: "What is your product called?", answerType: "text"},
-    {question: "When is your product used?", answerType: "text"},
-    {question: "What does your product accomplish?", answerType: "text"}
+    {
+      question: "What role best describes your clients?", 
+      answerType: "select", 
+      options: ["Paralegals", "Lawyers"]
+    },
+    {
+      question: "Where do your clients typically work?", 
+      answerType: "select", 
+      options: ["law firms", "inhouse legal departments"]
+    },
+    {
+      question: "What is your product called?", 
+      answerType: "textarea"
+    },
+    {
+      question: "When is your product used?", 
+      answerType: "textarea"
+    },
+    {
+      question: "What does your product accomplish?", 
+      answerType: "textarea"
+    }
   ]
   // answers: [],
   // madlib: `${this.answers[0]} at ${this.answers[1]} use ${this.answers[2]} when ${this.answers[3]} in order to ${this.answers[4]}`
