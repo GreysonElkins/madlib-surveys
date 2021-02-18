82c730bf019f70faf7bd7abf781cc786c2485f30
Greyson Elkins <greysonelkins@gmail.com>
2021-02-17 20:54:15 -0700

    Break into question cards

--- src/components/UserSurvey/UserSurvey.js
+++ src/components/UserSurvey/UserSurvey.js
@@ -9,6 +9,7 @@ import './UserSurvey.css'
 
 const UserSurvey = ({ survey, answerPreview }) => {
   const [answers, setAnswers] = useState(determineInitialFormState())
+  const [currentQuestion, setCurrentQuestion] = useState(1)
 
   function determineInitialFormState () {
     const state = {}
@@ -19,14 +20,18 @@ const UserSurvey = ({ survey, answerPreview }) => {
   }
 
   const renderQuestions = (errors, touched) => {
-    return survey.questions.map((question) => (
-      <Question 
-        question={question} 
-        key={`Question-${question.num}`}
-        errors={errors}
-        touched={touched}
-      />
-    ))
+    const question = survey.questions[currentQuestion - 1]
+    // {
+    //   return survey.questions.map((question) => (
+      return (
+        <Question 
+          question={question} 
+          key={`Question-${question.num}`}
+          errors={errors}
+          touched={touched}
+        />
+      )
+  //   ))
   }
 
   const updateStoredResponses = () => {
@@ -44,6 +49,11 @@ const UserSurvey = ({ survey, answerPreview }) => {
     return localStorage.getItem('responses') ? true : false
   }
 
+  const goToNextQuestion = () => {
+    const nextQuestion = currentQuestion + 1
+    setCurrentQuestion(nextQuestion)
+  }
+
   return (
     <>
     <h1>{survey.name}</h1>
@@ -57,15 +67,55 @@ const UserSurvey = ({ survey, answerPreview }) => {
           resetForm()
         }}
         >
-        {({ values, errors, touched }) => (
+        {({ values, errors, touched, validateForm, setFieldError, setTouched, setErrors }) => (
           <Form
           className="questions"
-          // onChange={
-          //   setAnswers(values)
-          // }
           >
             {renderQuestions(errors, touched)}
-            <button type="submit">Submit</button>
+            {/* I would love to move these into their own function, 
+              but I'm suspicious of passing them Formik bag methods without their corresponding "state" values,
+              so I'm leaving it for now to work on some other things.
+            */}
+            <div className="question-buttons">
+              {currentQuestion > 1 && 
+                <button 
+                  className="cta-3"
+                  type="button"
+                  onClick={() => {
+                    const previousQuestion = currentQuestion - 1
+                    setCurrentQuestion(previousQuestion)
+                  }}
+                >
+                  Previous
+                </button>
+              }
+              {currentQuestion === survey.questions.length ? 
+                <button 
+                  type="submit"
+                  className="cta-1"
+                >
+                  Submit
+                </button> :
+                <button 
+                  className="cta-2"
+                  type="button" 
+                  onClick={() => {
+                    setTouched({[`question-${currentQuestion}`]: true})
+                    validateForm().then(result => {
+                      if (!result[`question-${currentQuestion}`]) {
+                        goToNextQuestion()
+                        setAnswers(values)
+                        setErrors({})
+                      } else {
+                        setFieldError({field: `question-${currentQuestion}`, errorMsg: result[`question-${currentQuestion}`]})
+                      }
+                    })
+                  }}
+                >
+                  Next
+                </button>
+              }
+            </div>
           </Form>
         )}
       </Formik>
@@ -75,7 +125,6 @@ const UserSurvey = ({ survey, answerPreview }) => {
       />
     </div>
       {checkForResponses() &&
-        // gotta rerender this on submit
         <SurveyResponses currentAnswers={answers}/>
       }
       </>
